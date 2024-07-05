e3256332566ce822154d07f636437c16cb4cda31
BhavishyaA516 <115857780+BhavishyaA516@users.noreply.github.com>
2024-07-05 19:20:46 +0530

    Add files via upload

---
+++ chart/chart1.js
@@ -0,0 +1,28 @@
+const ctx1 = document.getElementById('barchart').getContext('2d');
+const barChart = new Chart(ctx1, {
+    type: 'bar',
+    data: {
+        labels: ['First Year', 'Second Year', 'Third Year'],
+        datasets: [{
+            label: 'Total Members: 131',
+            data: [49, 51, 31],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+});
